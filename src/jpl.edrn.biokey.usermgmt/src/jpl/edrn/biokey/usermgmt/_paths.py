c8bdbcb36734556b2fd24c5838afcd270c2a1250
Sean Kelly <kelly@seankelly.biz>
2024-06-17 15:41:17 -0500

    Respect FORCE_SCRIPT_NAME

--- src/jpl.edrn.biokey.usermgmt/src/jpl/edrn/biokey/usermgmt/_paths.py
+++ src/jpl.edrn.biokey.usermgmt/src/jpl/edrn/biokey/usermgmt/_paths.py
@@ -3,6 +3,7 @@
 '''🧬🔑🕴️ BioKey user management: path handling.'''
 
 
+from django.conf import settings
 from django.http import HttpRequest
 from wagtail.models import Site
 
@@ -18,5 +19,11 @@ def make_pwreset_url(slug: str, uid: str, token: str, request: HttpRequest) -> s
     base_url = f'{scheme}://{current_site.hostname}'
     if current_site.port and current_site.port not in (80, 443):
         base_url += f':{current_site.port}'
+    base_url += settings.FORCE_SCRIPT_NAME
 
     return f'{base_url}/pwreset/{slug}/{uid}/{token}'
+
+
+# In retrospect, this shouldn't be a view, but a child page of the
+# DirectoryInformationTree, so coming up with URLs like the above
+# wouldn't be necessary.
